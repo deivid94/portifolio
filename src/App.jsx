85d4ad6da1c687dbb6968bdd6e7777d6fccc6b0a
deivid94 <md031194@gmail.com>
2024-09-19 00:51:53 -0300

    about-me

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
-
 import './App.css'
 import Header from './components/Header'
+import Aboutme from './components/AboutMe'
 
 function App() {
  
 
   return (
+    <body>
     <Header />
-   
+    <Aboutme />
+    
+    </body>
+
   )
 }
 
---
+++ src/components/AboutMe/index.jsx
@@ -0,0 +1,27 @@
+import './about.css'
+export default function Aboutme(){
+    return (
+        <section className="aboutme_section">
+        <div className="about__me"> 
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet consectetur.
+            Semper lacus eget et consectetur egestas pharetra lectus.
+             Lorem cras scelerisque id dignissim phasellus. 
+             Sed turpis pellentesque gravida magna diam nunc porta eget.
+            Vitae donec ultricies faucibus id et enim porta adipiscing.</p>
+
+                  <a href="#"> Download CV </a>
+        </div>
+
+        <div className="aboutme__links">
+            <ul>
+                <li>Website Development</li>
+                <li>Webapp Development</li>
+                <li>Website Development</li>
+            </ul>
+        </div>
+
+        </section> 
+           
+    )
+}
