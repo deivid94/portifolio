edefe2e4f3994ad3a8fe2c889198b7a15fbd9acc
deivid94 <md031194@gmail.com>
2024-09-26 01:02:41 -0300

    layout display card skills

--- src/components/CardSkill/index.jsx
+++ src/components/CardSkill/index.jsx
@@ -1,14 +1,46 @@
 import './card.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faReact } from "@fortawesome/free-brands-svg-icons"
+import { faJava } from '@fortawesome/free-brands-svg-icons'
 
 export default function CardSkill (){
     return (
+        <section className='card_container'>
         <div className="card_skill">
-         <i className='card_skill__language fa-3x'>
-            <FontAwesomeIcon icon={faReact}  />
+         <i className='fa-3x'>
+            <FontAwesomeIcon icon={faReact}/>
          </i>
-
+          
         </div>
+
+        <div className='card_skill'>
+        <i className='fa-3x' >
+            <FontAwesomeIcon icon={faJava}/>
+        </i>
+        </div>
+
+        <div className='card_skill'>
+        <i className='fa-3x' >
+            <FontAwesomeIcon icon={faJava}/>
+        </i>
+        </div>
+
+        <div className='card_skill'>
+        <i className='fa-3x' >
+            <FontAwesomeIcon icon={faJava}/>
+        </i>
+        </div>
+        <div className='card_skill'>
+        <i className='fa-3x' >
+            <FontAwesomeIcon icon={faJava}/>
+        </i>
+        </div>
+        <div className='card_skill'>
+        <i className='fa-3x' >
+            <FontAwesomeIcon icon={faJava}/>
+        </i>
+        </div>
+
+        </section>
     )
 }
