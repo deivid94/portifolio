ebc08a428961c748b991ac7d228d7fc436f98cfc
deivid94 <md031194@gmail.com>
2024-10-02 00:28:20 -0300

    feat: adicionado cardskills com bordas

--- src/components/CardSkill/index.jsx
+++ src/components/CardSkill/index.jsx
@@ -2,42 +2,42 @@ import './card.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faReact } from "@fortawesome/free-brands-svg-icons"
 import { faJava } from '@fortawesome/free-brands-svg-icons'
+import { faJs } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faDocker } from '@fortawesome/free-brands-svg-icons'
+
+
 
 export default function CardSkill (){
     return (
         <section className='card_container'>
         <div className="card_skill">
-         <i className='fa-3x'>
-            <FontAwesomeIcon icon={faReact}/>
+         <i className='icon-fa fa-3x'>
+            <FontAwesomeIcon icon={faJava}/>
          </i>
-          
+
+        </div>
+
+        <div className='card_skill'>
+            <i className='fa-3x' >
+            <FontAwesomeIcon icon={faReact}/>
+            </i>
         </div>
 
         <div className='card_skill'>
         <i className='fa-3x' >
-            <FontAwesomeIcon icon={faJava}/>
+            <FontAwesomeIcon icon={faJs}/>
         </i>
         </div>
 
         <div className='card_skill'>
         <i className='fa-3x' >
-            <FontAwesomeIcon icon={faJava}/>
-        </i>
-        </div>
-
-        <div className='card_skill'>
-        <i className='fa-3x' >
-            <FontAwesomeIcon icon={faJava}/>
+            <FontAwesomeIcon icon={faGithub}/>
         </i>
         </div>
         <div className='card_skill'>
         <i className='fa-3x' >
-            <FontAwesomeIcon icon={faJava}/>
-        </i>
-        </div>
-        <div className='card_skill'>
-        <i className='fa-3x' >
-            <FontAwesomeIcon icon={faJava}/>
+            <FontAwesomeIcon icon={faDocker}/>
         </i>
         </div>
 
