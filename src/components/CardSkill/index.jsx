fc587d211aa476387ea324c862ebd8bb19013429
deivid94 <md031194@gmail.com>
2024-10-09 19:50:50 -0300

    feat: criacao do progressbar

--- src/components/CardSkill/index.jsx
+++ src/components/CardSkill/index.jsx
@@ -1,45 +1,78 @@
 import './card.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faReact } from "@fortawesome/free-brands-svg-icons"
-import { faJava } from '@fortawesome/free-brands-svg-icons'
-import { faJs } from '@fortawesome/free-brands-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faDocker } from '@fortawesome/free-brands-svg-icons'
+ import { faJava } from '@fortawesome/free-brands-svg-icons'
+ import { faJs } from '@fortawesome/free-brands-svg-icons'
+ import { faGithub } from '@fortawesome/free-brands-svg-icons'
+ import { faDocker } from '@fortawesome/free-brands-svg-icons'
+
 
 
 
 export default function CardSkill (){
     return (
+
+
         <section className='card_container'>
-        <div className="card_skill">
-         <i className='icon-fa fa-3x'>
-            <FontAwesomeIcon icon={faJava}/>
-         </i>
-
+        <div className="wrapper">
+            <div className="outer">
+                <div className="inner">
+                    <div className="icon">
+                        <i className='fa-3x'>
+                          <FontAwesomeIcon icon={ faReact} />  
+                        </i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="wrapper">
+            <div className="outer">
+                <div className="inner">
+                    <div className="icon">
+                        <i className='fa-3x'>
+                          <FontAwesomeIcon icon={ faJava} />  
+                        </i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="wrapper">
+            <div className="outer">
+                <div className="inner">
+                    <div className="icon">
+                        <i className='fa-3x'>
+                          <FontAwesomeIcon icon={ faJs} />  
+                        </i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="wrapper">
+            <div className="outer">
+                <div className="inner">
+                    <div className="icon">
+                        <i className='fa-3x'>
+                          <FontAwesomeIcon icon={ faGithub} />  
+                        </i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="wrapper">
+            <div className="outer">
+                <div className="inner">
+                    <div className="icon">
+                        <i className='fa-3x'>
+                          <FontAwesomeIcon icon={ faDocker} />  
+                        </i>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div className='card_skill'>
-            <i className='fa-3x' >
-            <FontAwesomeIcon icon={faReact}/>
-            </i>
-        </div>
+        
 
-        <div className='card_skill'>
-        <i className='fa-3x' >
-            <FontAwesomeIcon icon={faJs}/>
-        </i>
-        </div>
 
-        <div className='card_skill'>
-        <i className='fa-3x' >
-            <FontAwesomeIcon icon={faGithub}/>
-        </i>
-        </div>
-        <div className='card_skill'>
-        <i className='fa-3x' >
-            <FontAwesomeIcon icon={faDocker}/>
-        </i>
-        </div>
 
         </section>
     )
