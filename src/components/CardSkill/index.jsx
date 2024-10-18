7cdbbfe0dde48fa0c666dba2fc663dfc9bcc5820
deivid94 <md031194@gmail.com>
2024-10-17 23:49:45 -0300

    loading bar with icon

--- src/components/CardSkill/index.jsx
+++ src/components/CardSkill/index.jsx
@@ -15,6 +15,35 @@ export default function CardSkill (){
 
         <section className='card_container'>
           <div className="wrapper">
+            <i className='svgLanguage fa-3x'>
+            <FontAwesomeIcon icon={faJava} />
+            </i>
+            <div className="outer">
+          
+                <div className="inner">
+              
+                                
+                    <div id="icon">
+                           
+                            <svg width="350px" height="350px">
+                            <defs>
+                            <radialGradient id="linearGradient">
+                                <stop offset="0%" stopColor='#6f61ff' />
+                                <stop offset="55%" stopColor='#9a90ff' />
+                                <stop offset="100%" stopColor='#6f61ff' />
+                            </radialGradient>
+                            </defs> 
+                            <circle cx="175" cy="176" r="82"/>
+                            </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+        <div className="wrapper">
+        <i className='svgLanguage fa-3x'>
+            <FontAwesomeIcon icon={faReact} />
+            </i>
             <div className="outer">
                 <div className="inner">
                     <div id="icon">
@@ -35,26 +64,9 @@ export default function CardSkill (){
         </div>
        
         <div className="wrapper">
-            <div className="outer">
-                <div className="inner">
-                    <div id="icon">
-                        
-                            <svg width="350px" height="350px">
-                            <defs>
-                            <radialGradient id="linearGradient">
-                                <stop offset="0%" stopColor='#6f61ff' />
-                                <stop offset="55%" stopColor='#9a90ff' />
-                                <stop offset="100%" stopColor='#6f61ff' />
-                            </radialGradient>
-                            </defs> 
-                            <circle cx="175" cy="176" r="82"/>
-                            </svg>
-                    </div>
-                </div>
-            </div>
-        </div>
-       
-        <div className="wrapper">
+        <i className='svgLanguage fa-3x'>
+            <FontAwesomeIcon icon={faJs} />
+            </i>
             <div className="outer">
                 <div className="inner">
                     <div id="icon">    
@@ -75,11 +87,41 @@ export default function CardSkill (){
         </div>
        
         <div className="wrapper">
+        <i className='svgLanguage fa-3x'>
+            <FontAwesomeIcon icon={faGithub} />
+            </i>
             <div className="outer">
                 <div className="inner">
                     
                     <div id="icon">
-                             
+                            
+                            <svg width="350px" height="350px">
+                            <defs>
+                            <radialGradient id="linearGradient">
+                                <stop offset="0%" stopColor='#6f61ff' />
+                                <stop offset="55%" stopColor='#9a90ff' />
+                                <stop offset="100%" stopColor='#6f61ff' />
+                            </radialGradient>
+                            </defs> 
+                            <circle cx="175" cy="176" r="82"/>
+                            </svg>
+                            
+                                                         
+                            
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="wrapper">
+        <i className='svgLanguage fa-3x'>
+            <FontAwesomeIcon icon={faDocker} />
+            </i>
+            <div className="outer">
+                <div className="inner">
+                    
+                    <div id="icon">
+                            
                             <svg width="350px" height="350px">
                             <defs>
                             <radialGradient id="linearGradient">
